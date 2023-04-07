962f5757426fafb12c9addca780eeac4ef651125
misterdan100 <danielmca60@gmail.com>
2023-04-07 16:16:59 -0500

    codigo clase 13 - eventos desde html a JS para interactuar con el usuario

--- script.js
+++ script.js
@@ -1,32 +1,10 @@
 const h1 = document.querySelector("h1");
-const p = document.querySelector("p");
-const parrafito = document.querySelector(".parrafito");
-const pid = document.querySelector("#pid")
-const input = document.querySelector("input");
+const input1 = document.querySelector("#calculo1");
+const input2 = document.querySelector("#calculo2");
+const btn = document.querySelector("#btnCalcular");
+const pResult = document.querySelector("#result");
 
-console.log(h1)
-
-console.log({
-        h1,
-        p,
-        parrafito,
-        pid,
-        input
-    }
-)
-
-h1.innerHTML = "Patito <br> alegre";
-h1.innerText = "Patito <br> feliz";
-// console.log(h1.getAttribute("class"))
-// console.log(h1.setAttribute("class", "rojo"))
-
-h1.classList.add("rojo")
-h1.classList.remove("verde")
-
-input.value = "456";
-
-pid.innerHTML = "";
-const img = document.createElement("img");
-img.setAttribute("src", "https://source.unsplash.com/55ijBDBaz_k")
-
-pid.appendChild(img)
+function btnOnClick() {
+    const resultado = parseInt(input1.value) + parseInt(input2.value);
+    pResult.innerText = "El resultado es: " + resultado;
+}
